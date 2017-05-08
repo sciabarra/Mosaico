6099af5c310fd4ab17cf0d904a5a84bbed5b5af1
Michele Sciabarra <michele+youtrack@sciabarra.com>
2017-05-08 13:08:43 +0200

    fix spark starter

--- docker/alpine/spark/run.sh
+++ docker/alpine/spark/run.sh
@@ -3,14 +3,13 @@
 if ! grep 8.8.8.8 /etc/resolv.conf
 then echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
 fi
-CONF=/usr/spark/conf/spark-env.sh
+export CONF=/usr/spark/conf/spark-env.sh
 echo export SPARK_HOME=/usr/spark >$CONF
 echo export HADOOP_HOME=/usr/hadoop >>$CONF
-echo export PATH=\"$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH\">>$CONF
-echo export SPARK_DIST_CLASSPATH=\"$(hadoop classpath)\" >>$CONF
+echo export PATH=\"/usr/hadoop/bin:/usr/spark/bin:$PATH\" >>$CONF
+echo export SPARK_DIST_CLASSPATH=\"$(/usr/hadoop/bin/hadoop classpath)\" >>$CONF
 echo export SPARK_DAEMON_MEMORY=1000m >>$CONF
 echo export SPARK_WORKER_MEMORY=1000m >>$CONF
-
 chmod +x $CONF
 cd /usr/spark
 if test -z "$SPARK_MASTER"
