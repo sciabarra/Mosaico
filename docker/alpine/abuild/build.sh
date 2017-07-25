bdaf861f6fea8a5f4b8beff841d253c40fb9cd6b
Michele Sciabarra <michele+youtrack@sciabarra.com>
2017-07-25 15:35:19 +0200

    chown output file

--- docker/alpine/abuild/build.sh
+++ docker/alpine/abuild/build.sh
@@ -3,4 +3,5 @@ cp /home/abuild/$1 /home/packager/APKBUILD
 cd /home/packager
 abuild checksum
 abuild -R
-cp packages/home/*/*.apk /home/target/$2
+sudo cp packages/home/*/*.apk "/home/target/$2"
+sudo chown $(stat -c "%u:%g" /home/target) "/home/target/$2"
